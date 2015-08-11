904586d3b2442ea5db3a32c4721651dbf7daf4c4
Fichberg <fichberg@blackfish.fichberg>
2015-08-11 14:11:19 -0300

    Added system call to print working directory, using getcwd function, from <unistd.h>.

--- ep1sh.c
+++ ep1sh.c
@@ -1,34 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 void removenl(char *);
 
 int main(int argc, char **argv)
 {
+  char wd[256];
   char cmd[16];
-
-  /*funções da família "exec" quebram o loop. Soluções:
-  1 - usar "system"
-  2 - fazer um fork e depois usar as funções exec
-
-  Abaixo, a opção 1
-  */
-
+  
   while(1){
-    fflush(stdin);
-    printf("$ ");
-    fgets(cmd, 15, stdin);
-    removenl(cmd);
-    if(strcmp(cmd, "/bin/ls -l") == 0) {
-      if(system("/bin/ls -l") != 0){
-        printf("Command not supported.\n");
-        return 1;
+    if(getcwd(wd, sizeof(wd)) != NULL) {
+      fflush(stdin);
+      printf("[%s] ", wd);
+      fgets(cmd, 15, stdin);
+      removenl(cmd);
+      if(strcmp(cmd, "/bin/ls -l") == 0) {
+        if(system("/bin/ls -l") != 0){
+          printf("Command not supported.\n");
+          return 1;
+        }
       }
+      else printf("Unrecognized command \"%s\".\n", cmd);
     }
-    else printf("Unrecognized command \"%s\".\n", cmd);
+    else perror("Error (getcwd)");
   }
   return 0;
+
 }
 
 void removenl(char *str)
--- makefile
+++ makefile
@@ -3,3 +3,8 @@ ep1sh: ep1sh.o
 
 ep1sh.o: ep1sh.c
 	gcc -c ep1sh.c -Wall -ansi -O2
+
+clean:
+	rm -rf *.o
+	rm -rf *~
+	rm ep1sh
