5a79fa9a253fdbf3381ee3cf555a6d67ad0c7a07
Fichberg <fichberg@blackfish.fichberg>
2015-08-11 14:43:25 -0300

    cd system call implemented using chdir() function, from <unistd.h>.

--- ep1sh.c
+++ ep1sh.c
@@ -9,7 +9,7 @@ int main(int argc, char **argv)
 {
   char wd[256];
   char cmd[16];
-  
+
   while(1){
     if(getcwd(wd, sizeof(wd)) != NULL) {
       fflush(stdin);
@@ -22,9 +22,24 @@ int main(int argc, char **argv)
           return 1;
         }
       }
+      else if(strncmp(cmd, "cd ", 3) == 0) {
+        char *cdarg, *path; int i;
+
+        cdarg = malloc((strlen(cmd) - 2) * sizeof(*cdarg));
+
+        for(i = 3; i < strlen(cmd); i++) cdarg[i - 3] = cmd[i];
+        cdarg[i] = '\0';
+
+        path = malloc((strlen(wd) + strlen(cdarg) + 2) * sizeof(*path));
+        strcpy(path, wd); strcat(path, "/"); strcat(path, cdarg);
+        if(chdir(path) != 0)
+          printf("Error changing directory. Make sure directory exists.\n");
+
+        free(cdarg); cdarg = NULL;
+      }
       else printf("Unrecognized command \"%s\".\n", cmd);
     }
-    else perror("Error (getcwd)");
+    else perror("Error (getcwd).\n");
   }
   return 0;
 
