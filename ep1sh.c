26a839b68667e4383b5b65aefd468bc6be337e70
Fichberg <fichberg@blackfish.fichberg>
2015-08-13 15:14:41 -0300

    Organized code and added comments.

--- ep1sh.c
+++ ep1sh.c
@@ -4,8 +4,15 @@
 #include <unistd.h>
 #include <readline/history.h>
 
+/*Prototypes*/
 void removenl(char *);
+void getcmd(char *);
+int cmdls(char *);
+int cmdcd(char *, char *);
+void unrecognized(char *);
+/*************/
 
+/*ep1sh main loop.*/
 int main(int argc, char **argv)
 {
   char wd[256];
@@ -13,59 +20,85 @@ int main(int argc, char **argv)
 
   while(1){
     if(getcwd(wd, sizeof(wd)) != NULL) {
-      fflush(stdin);
       printf("[%s] ", wd);
-      fgets(cmd, 255, stdin);
-      removenl(cmd);
-      if(strcmp(cmd, "/bin/ls -l") == 0) {
-        if(system("/bin/ls -l") != 0){
-          printf("Command not supported.\n");
-          return 1;
-        }
-      }
-      else if(strncmp(cmd, "cd ", 3) == 0 && strlen(cmd) > 3) {
-        char *path; int i;
+      getcmd(cmd);
 
-        if(cmd[3] != '/') {
-          char *cdarg;
-          cdarg = malloc((strlen(cmd) - 2) * sizeof(*cdarg));
-
-          for(i = 3; i < strlen(cmd); i++) cdarg[i - 3] = cmd[i];
-          cdarg[i] = '\0';
-
-          path = malloc((strlen(wd) + strlen(cdarg) + 2) * sizeof(*path));
-          strcpy(path, wd); strcat(path, "/"); strcat(path, cdarg);
-
-          if(chdir(path) != 0) printf("Bad argument for 'cd'.\n");
-
-          free(cdarg); cdarg = NULL;
-        }
-        else {
-          path = malloc((strlen(cmd) - 2) * sizeof(*path));
-
-          for(i = 3; i < strlen(cmd); i++) path[i - 3] = cmd[i];
-          path[i] = '\0';
-
-          if(chdir(path) != 0) printf("Bad argument for 'cd'.\n");
-        }
-        free(path); path = NULL;
-      }
-      else if(strncmp(cmd, "cd", 2) == 0) printf("Bad argument for 'cd'.\n");
-      else printf("Unrecognized command \"%s\".\n", cmd);
+      if(cmdls(cmd));
+      else if(cmdcd(wd, cmd));
+      else unrecognized(cmd);
     }
     else perror("Error (getcwd).\n");
   }
   return 0;
-
 }
 
+/*User invoked unknown command to ep1sh*/
+void unrecognized(char *cmd)
+{
+  printf("Unrecognized command \"%s\".\n", cmd);
+}
+
+/*Check if user invoked cd command to ep1sh*/
+int cmdcd(char *wd, char *cmd)
+{
+  if(strncmp(cmd, "cd ", 3) == 0 && strlen(cmd) > 3) {
+    char *path; int i;
+    if(cmd[3] != '/') {
+      char *cdarg;
+      cdarg = malloc((strlen(cmd) - 2) * sizeof(*cdarg));
+
+      for(i = 3; i < strlen(cmd); i++) cdarg[i - 3] = cmd[i];
+      cdarg[i] = '\0';
+
+      path = malloc((strlen(wd) + strlen(cdarg) + 2) * sizeof(*path));
+      strcpy(path, wd); strcat(path, "/"); strcat(path, cdarg);
+
+      if(chdir(path) != 0) printf("Bad argument for 'cd'.\n");
+
+      free(cdarg); cdarg = NULL;
+    }
+    else {
+      path = malloc((strlen(cmd) - 2) * sizeof(*path));
+
+      for(i = 3; i < strlen(cmd); i++) path[i - 3] = cmd[i];
+      path[i] = '\0';
+
+      if(chdir(path) != 0) printf("Bad argument for 'cd'.\n");
+    }
+    free(path); path = NULL;
+    return 1;
+  }
+  else if(strncmp(cmd, "cd", 2) == 0) {
+    printf("Bad argument for 'cd'.\n");
+    return 1;
+  }
+  return 0;
+}
+
+/*Check if user invoked /bin/ls -l command to ep1sh*/
+int cmdls(char *cmd)
+{
+  if(strcmp(cmd, "/bin/ls -l") == 0) {
+    if(system("/bin/ls -l") != 0) printf("Command not supported.\n");
+    return 1;
+  }
+  return 0;
+}
+
+/*Get user command*/
+void getcmd(char *cmd)
+{
+  fflush(stdin);
+  fgets(cmd, 255, stdin);
+  removenl(cmd);
+}
+
+/*Remove final character \n from user command*/
 void removenl(char *str)
 {
   if ((strlen(str) > 0) && (str[strlen(str) - 1] == '\n'))
         str[strlen(str) - 1] = '\0';
 }
-
-
 
 /*
 Notes:
