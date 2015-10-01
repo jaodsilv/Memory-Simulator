a7cac51e5bec3a0d30863b84f6d1872ea01a62a1
Fichberg <rfichberg@gmail.com>
2015-09-30 22:07:04 -0300

    Free List source. Missing some TODOs related to the 'fits' policies.

--- headers/process.h
+++ headers/process.h
@@ -6,24 +6,21 @@
 #define END 1
 #define ACCESS 2
 
-typedef struct EVENT *event;
-typedef struct PROCESS *process;
-
-struct EVENT {
+typedef struct event {
 	int eventType;
 	int time;
 	int position;
 	int64_t PID;
-	event next;
-};
+	struct event *next;
+} Event;
 
-struct PROCESS {
+typedef struct process {
 	int64_t PID; /* 64 bit PID */
-	int mem_pos_phy;
-	int mem_pos_vir;
-	int mem_size;
+	unsigned int mem_pos_phy;
+	unsigned int mem_pos_vir;
+	unsigned int size;
 	char *nome;
-};
+} Process;
 
 process create_process(char* name, int mem_size);
 event create_event(int eventType, int time, int position, int PID);
