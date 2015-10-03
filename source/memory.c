de8de16f28720ef5b6c4b7e3c4a50113fad1b5c8
Fichberg <rfichberg@gmail.com>
2015-10-03 18:45:03 -0300

    Printer almost finished.

--- source/memory.c
+++ source/memory.c
@@ -61,22 +61,68 @@ void *run(void *args)
     }
   }
   if(thread->role == PRINTER) {
+    FILE *mfile, *mfile_u;
     float last = 0, t = 0, ret;
+    int8_t *physical_array, *virtual_array;
+    uint8_t *physical_array_u, *virtual_array_u;
+
+    physical_array = malloc(total * sizeof(*physical_array));
+    virtual_array = malloc(virtual * sizeof(*virtual_array));
+    physical_array_u = malloc(total * sizeof(*physical_array_u));
+    virtual_array_u = malloc(virtual * sizeof(*virtual_array_u));
     printf("I am the printer thread!\n");
     /*Wait Timer thread starts the simulation*/
     while(elapsed_time == -1) continue;
     while(simulating) {
-      /*Idea: Assign atomically to ret first and then do a local comparission between ret and last to avoid using a semaphore*/
+      /*IDEA: Assign atomically to ret first and then do a local comparission between ret and last to avoid using a semaphore*/
       ret = elapsed_time;
       if(last != ret) {
         last = ret; t += 0.1;
         if(t >= thread->intrvl) {
-          t = 0;
-          printf("Printer: time is %.1f\n", last);
-          /*Do grab lock and print_memory here*/
+          unsigned int i; t = 0;
+          /*TODO: grab mutex here*/
+
+          /*IDEA: In order to operate considering the initial -1 value, we need to have
+          both signed and unsigned integers of size 1b (as the enunciation of the EP commanded),
+          this because unsigned goes from [0, 255] and -1 is out of the interval. Thus,
+          we need another file pointer as well. Whenever there is a -1 in a position of the
+          file, we use the signed values and file pointer to print it, otherwise, we use the
+          unsigned ones*/
+
+          /*Read physical binary file*/
+          mfile = fopen("/tmp/ep2.mem", "rb");
+          mfile_u = fopen("/tmp/ep2.mem", "rb");
+          fread(physical_array, sizeof(int8_t), (size_t)total, mfile);
+          fread(physical_array_u, sizeof(uint8_t), (size_t)total, mfile_u);
+
+          /*Prints physical memory file*/
+          for(i = 0; i < total; i++) {
+            if(physical_array[i] < 0) printf("-1 ");
+            else printf("%u ", physical_array_u[i]);
+          } printf("\n");
+          fclose(mfile); fclose(mfile_u);
+
+          /*Read virtual binary file*/
+          mfile = fopen("/tmp/ep2.vir", "rb");
+          mfile_u = fopen("/tmp/ep2.vir", "rb");
+          fread(virtual_array, sizeof(int8_t), (size_t)virtual, mfile);
+          fread(virtual_array_u, sizeof(uint8_t), (size_t)virtual, mfile_u);
+
+          /*Prints physical memory file*/
+          for(i = 0; i < virtual; i++) {
+            if(virtual_array[i] < 0) printf("-1 ");
+            else printf("%u ", virtual_array_u[i]);
+          } printf("\n");
+          fclose(mfile); fclose(mfile_u);
+
+          /*TODO: release mutex here*/
         }
       }
     }
+    free(physical_array); physical_array = NULL;
+    free(virtual_array); virtual_array = NULL;
+    free(physical_array_u); physical_array_u = NULL;
+    free(virtual_array_u); virtual_array_u = NULL;
   }
   if(thread->role == TIMER) {
     float t = 0;
@@ -88,7 +134,7 @@ void *run(void *args)
       clock_gettime(CLOCK_MONOTONIC, &now);
       /*t is incremented by 0.1 every 0.1s*/
       if(abs(now.tv_nsec - range.tv_nsec) > 100000000) {
-        /*Idea: increment a local 't' to assign time global 'elapsed_time' atomically and avoid the use of a semaphore*/
+        /*IDEA: increment a local 't' to assign time global 'elapsed_time' atomically and avoid the use of a semaphore*/
         t += 0.1; elapsed_time = t;
         /*Restart range*/
         clock_gettime(CLOCK_MONOTONIC, &range);
@@ -102,16 +148,23 @@ void *run(void *args)
 void create_memory(int type)
 {
   FILE *mfile;
-  int8_t n = -1, *unused = &n;
+  int8_t *n1, *n2;
 
   switch(type) {
+    unsigned int i;
     case PHYSICAL:
+      n1 = malloc(total * sizeof(*n1));
+      for(i = 0; i < total; i++) n1[i] = -1;
       mfile = fopen("/tmp/ep2.mem", "wb");
-      fwrite(unused, sizeof(*unused), (size_t)total, mfile);
+      fwrite(n1, sizeof(n1[0]), total * sizeof(n1[0]), mfile);
+      free(n1); n1 = NULL;
       break;
     case VIRTUAL:
+      n2 = malloc(virtual * sizeof(*n2));
+      for(i = 0; i < virtual; i++) n2[i] = -1;
       mfile = fopen("/tmp/ep2.vir", "wb");
-      fwrite(unused, sizeof(*unused), (size_t)virtual, mfile);
+      fwrite(n2, sizeof(n2[0]), virtual * sizeof(n2[0]), mfile);
+      free(n2); n2 = NULL;
       break;
   }
   fclose(mfile);
